0cee96f35616b39454e28d2e2a2bc91d877fb31c
DemonKuchiki <kjeldkuchiki@hotmail.com>
2014-10-03 15:15:10 +0200

    second push part 2
    a

---
+++ Assets/Scripts/arm.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arm : MonoBehaviour {
+	public Globals global;
+	void Start (){
+		global = GameObject.Find("GameManager").GetComponent<Globals>();
+	}
+
+	public void addForceToArm(int multiplier){
+		transform.rigidbody.AddForce(Vector3.up *(200*multiplier));
+	}
+	void OnTriggerEnter(Collider collision){
+		if(collision.gameObject.tag == "AiTrigger")
+		{
+			global.AiPush = true;
+		}
+	}
+}
